52398023d60a659219bdc34a63467f9a364849ee
Jorge Borges <jorgetrabalho2016@gmail.com>
2020-09-22 16:17:20 -0300

    Utilizando req.params.id para capturar valor de URL

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -6,4 +6,18 @@ router.get("/", (req,res) => {
 	res.send()
 })
 
+router.post("/", (req,res) => {
+	console.log(req.body);
+	res.status(200).json(req.body);
+})
+
+// recebendo parametro pela URL
+
+router.get("/:id", (req,res) => {
+	console.log(req.params.id); // capturando valor da URL e imprimindo no console.
+	res.send(`ID: ${req.params.id}`); // enviando valor de parâmetro para o body.
+})
+
+
+
 module.exports = router;
