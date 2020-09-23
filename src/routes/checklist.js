74d0d5d4d4e22fde3226488674870f3746447ab9
Jorge Borges <jorgetrabalho2016@gmail.com>
2020-09-23 13:06:30 -0300

    Criando as rotas delete e put para checklist

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router(); // uma ferramenta do express;
-
 const Checklist = require("../models/checklist")
-
 
 // Nesta rota iremos retornar todos os checklist presente no banco
 
@@ -15,7 +13,24 @@ router.get("/", async (req,res) => {
 	}
 })
 
+router.put('/:id', async (req,res) => {
+	let {name} = req.body;
+	try{
+		let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}); // passando-se o parâmetro new com valor true podemos estar passando a atualização no resultado não o 
+		res.status(200).json(checklist);
+	}catch (error){
+		res.status(422).json(error);
+	}
+})
 
+router.delete('/:id', async (req,res) => {
+	try{
+		let checklist = await Checklist.findByIdAndRemove(req.params.id); 
+		res.status(200).json({"checklist": `${req.params.id} - Deleltada`}); 
+	}catch (error){
+		res.status(422).json(error);
+	}
+})
 
 router.post("/", async (req,res) => {
 	let {name} = req.body;
